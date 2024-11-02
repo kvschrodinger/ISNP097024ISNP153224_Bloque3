27f6a662d43949aac4324de958864d44de35ea87
Carlos Cuellar <isnp153224@ugb.edu.sv>
2024-11-02 13:25:33 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ISNP097024ISNP153224_Bloque3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ISNP097024ISNP153224_Bloque3.Models
+{
+    public class Pelicula
+    {
+        [Key]
+        public int IdPelicula { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Sipnosis { get; set; }
+        public string Duracion { get; set; }
+        public string Clasificacion { get; set; }
+    }
+}
